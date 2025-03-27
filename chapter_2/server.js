f35109f8c7c531ebe9585b579aa3fbfbde3ddb0c
Rogenecarl <chessgame221@gmail.com>
2025-03-27 16:52:33 +0800

    1:24:16

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -2,14 +2,35 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const data = {
+    name: 'James',
+    age: 24
+}
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
     console.log('yey hehe', req.method)
-    res.send('<h1>Homepage</h1>')
+    res.send(`<body style="background: lightblue; color: black"> 
+        <h1>DATA:</h1>
+        <p>${JSON.stringify(data)}</p>
+        </body>`)
 })
 
-app.get('/dashboard' , (req, res) => {
+app.get('/dashboard', (req, res) => {
     console.log('You are in the dashboard')
     res.send('<h1>dashboard</h1>')
+})
+
+app.get('/api/data', (req, res) => {
+    console.log('this are all the data')
+    res.send(data)
+})
+
+app.post('/api/data', (req, res) => {
+     const newEntry = req.body
+     console.log(newEntry)
+     res.sendStatus(201)
 })
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
