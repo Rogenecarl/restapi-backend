5250b037b0d7741b07f60221339ce2d0850acecc
Rogenecarl <chessgame221@gmail.com>
2025-03-27 16:11:36 +0800

    Basic backend project

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -2,5 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.get('/', (req, res) => {
+    console.log('yey hehe', req.method)
+    res.send('<h1>Homepage</h1>')
+})
+
+app.get('/dashboard' , (req, res) => {
+    console.log('You are in the dashboard')
+    res.send('<h1>dashboard</h1>')
+})
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
